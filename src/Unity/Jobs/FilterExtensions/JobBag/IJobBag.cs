da33b94a08ed1f42db140ced345e48db28461839
Serval1024 <serval1024@gmail.com>
2024-04-06 18:58:35 +1000

    Unity support migrate to own repository.

--- src/Unity/Jobs/FilterExtensions/JobBag/IJobBag.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AnotherECS.Unity.Jobs
-{
-    public interface IJobBag
-    {
-        int Count { get; }
-        void BeginForEachIndex(int chunkIndex);
-        void EndForEachIndex();
-    }
-}
--- src/Unity/Physics/UnityPhysicsFeature/Systems/EntityConst.cs
+++
@@ -1,9 +0,0 @@
-using EntityId = System.Int32;
-
-namespace AnotherECS.Physics
-{
-    public struct EntityConst
-    {
-        public const EntityId Zero = 0;
-    }
-}
